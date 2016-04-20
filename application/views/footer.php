7cb6ecfe14f1a1137a4845b8e2159d35863a81c2
Andy Broomfield <andy.broomfield@kitchendoorworkshop.com>
2016-04-20 13:17:13 +0100

    adding footer links

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,5 +1,7 @@
     <footer>
+        
       <p>Test Wiki : Content &copy <?php date('Y'); ?> the authors.</p>
+      <p><a href="home/">Home page</a> | <a href="pages/">All Pages</a></p>
     </footer>
 
   </div>
