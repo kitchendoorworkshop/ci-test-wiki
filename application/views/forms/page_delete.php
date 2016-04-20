f8b5f9d4a2d420495735b63090f24a758b348143
Andy Broomfield <andy.broomfield@kitchendoorworkshop.com>
2016-04-20 12:52:20 +0100

    changing cancel link on delete page to relative to base instead of docroot

--- application/views/forms/page_delete.php
+++ application/views/forms/page_delete.php
@@ -5,6 +5,6 @@
 	<p>Really delete <?php print $page['title']; ?></p>
 
 	<input type="submit" name="delete" value="Delete" />
-  <a href="/<?php print $page['slug']; ?>">Cancel</a>
+  <a href="<?php print $page['slug']; ?>">Cancel</a>
 
 </form>
