9e4cdfd4356cfbfabb38f2247f0e76e0d336d818
Andy Broomfield <andy.broomfield@kitchendoorworkshop.com>
2016-04-20 16:17:24 +0100

    adding hidden field slug to forms/page_create.php

--- application/views/forms/page_create.php
+++ application/views/forms/page_create.php
@@ -5,6 +5,10 @@
 	<label for="title">Title</label>
 	<input type="input" name="title" /><br />
 
+	<?php if ($slug): ?>
+		<input type="hidden" name="slug" value="<?php print $slug; ?>" />
+	<?php endif; ?>
+
 	<label for="content">Content</label>
 	<textarea name="content" rows="20" cols="60"></textarea><br />
 
