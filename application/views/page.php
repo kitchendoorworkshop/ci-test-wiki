6cd0f4c81c090d1ab73476427f518324e56e8fd8
Andy Broomfield <andy.broomfield@kitchendoorworkshop.com>
2016-04-20 16:12:13 +0100

    adding support in application/views/page.php for hiding edit links on missing page

--- application/views/page.php
+++ application/views/page.php
@@ -1,6 +1,9 @@
 <div id="content">
   <?php print $page['content']; ?>
 </div>
-<div id="functions">
-  <a href="page/<?php print $page['slug']; ?>/edit">Edit this page</a> | <a href="page/<?php print $page['slug']; ?>/delete">Delete this page</a>
-</div>
+
+<?php if ($show_edit_links): ?>
+    <div id="functions">
+      <a href="page/<?php print $page['slug']; ?>/edit">Edit this page</a> | <a href="page/<?php print $page['slug']; ?>/delete">Delete this page</a>
+    </div>
+<?php endif; ?>
